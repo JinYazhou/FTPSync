b95fcf53afa7e058248807e686f879da55920d71
jinyazhou <jin_yazhou@163.com>
2021-02-20 17:54:44 +0800

    no message

---
+++ FTPSync/FtpClient.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace FTPSync
+{
+    public class FtpClient
+    {
+        public static string _hostAddress = "ftp://127.0.0.1";
+        public static string _username = "jyz";
+        public static string _password = "jyz";
+        private static FtpWebRequest _ftpRequest = null;
+        private static FtpWebResponse _ftpResponse = null;
+
+        //// Constructor
+        //public FtpClient(string hostAddress, string username, string password)
+        //{
+        //    _hostAddress = hostAddress;
+        //    _username = username;
+        //    _password = password;
+        //}
+
+        /// <summary>
+        /// Download File
+        /// </summary>
+        /// <param name="remoteFile"></param>
+        /// <param name="localFile"></param>
+        public static void DownloadFile(string remoteFile, string localFile)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Credentials = new NetworkCredential(_username, _password);
+                    client.DownloadFile((_hostAddress + "/" + remoteFile).Trim(), localFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Upload File
+        /// </summary>
+        /// <param name="remoteFile"></param>
+        /// <param name="localFile"></param>
+        public static void UploadFile(string remoteFile, string localFile)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Credentials = new NetworkCredential(_username, _password);
+                    client.UploadFile((_hostAddress + "/" + remoteFile).Trim(), localFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Delete File
+        /// </summary>
+        /// <param name="deleteFile"></param>
+        public static string DeleteFile(string deleteFile)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + deleteFile);
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.DeleteFile;
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    return _ftpResponse.StatusDescription;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Delete Dir
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        public static string DeleteDir(string directoryPath)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + directoryPath);
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.RemoveDirectory;
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    return _ftpResponse.StatusDescription;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Rename File
+        /// </summary>
+        /// <param name="currentFileNameAndPath"></param>
+        /// <param name="newFileName"></param>
+        public static string RenameFile(string currentFileNameAndPath, string newFileName)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + currentFileNameAndPath);
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.Rename;
+                // Rename the File
+                _ftpRequest.RenameTo = newFileName;
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    return _ftpResponse.StatusDescription;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Create a New Directory on the FTP Server
+        /// </summary>
+        /// <param name="newDirectory"></param>
+        public static string CreateDir(string newDirectory)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + newDirectory);
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    return _ftpResponse.StatusDescription;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Get the Date/Time a File was Created
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static DateTime GetFileCreatedDateTime(string fileName)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + fileName);
+
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.GetDateTimestamp;
+
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    return _ftpResponse.LastModified;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Get the Size of a File
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static long GetFileSize(string fileName)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + fileName);
+
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.GetFileSize;
+
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    return _ftpResponse.ContentLength;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// List Directory Contents File/Folder Name Only
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
+        public static IList<string> DirectoryListSimple(string directory)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + directory);
+
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    StreamReader streamReader = new StreamReader(_ftpResponse.GetResponseStream());
+
+                    IList<string> directories = new List<string>();
+
+                    string line = streamReader.ReadLine();
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        directories.Add(line);
+                        line = streamReader.ReadLine();
+                    }
+
+                    return directories;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// List Directory Contents in Detail (Name, Size, Created, etc.)
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
+        public static IList<string> DirectoryListDetailed(string directory)
+        {
+            try
+            {
+                // Create an FTP Request
+                _ftpRequest = (FtpWebRequest)WebRequest.Create(_hostAddress + "/" + directory);
+
+                // Log in to the FTP Server with the User Name and Password Provided
+                _ftpRequest.Credentials = new NetworkCredential(_username, _password);
+
+                // Specify the Type of FTP Request
+                _ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+                // Establish Return Communication with the FTP Server
+                using (_ftpResponse = (FtpWebResponse)_ftpRequest.GetResponse())
+                {
+                    StreamReader streamReader = new StreamReader(_ftpResponse.GetResponseStream());
+
+                    IList<string> directories = new List<string>();
+
+                    string line = streamReader.ReadLine();
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        directories.Add(line);
+                        line = streamReader.ReadLine();
+                    }
+                    return directories;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ FTPSync/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace FTPSync
+{
+    class Program
+    {
+        private static readonly string _basePath = "/Users/jinyazhou/Documents/Site/";
+
+        //新旧文件夹字典
+        private static List<string> _oldFolderList = new List<string>();
+        private static List<string> _newFolderList = new List<string>();
+        //新旧文件字典
+        private static Dictionary<string, string> _oldFileList = new Dictionary<string, string>();
+        private static Dictionary<string, string> _newFileList = new Dictionary<string, string>();
+        private static Timer _syncTimer = null;
+        private static bool _workDown = true;
+
+        static void Main()
+        {
+            try
+            {
+                _syncTimer = new Timer(SyncEvent, null, 0, 10000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+            _syncTimer.Dispose();
+        }
+
+        static void SyncEvent(object obj)
+        {
+            if (!_workDown)
+            {
+                return;
+            }
+
+            _workDown = false;
+
+            _newFolderList = new List<string>();
+            _newFileList = new Dictionary<string, string>();
+            
+            _newFolderList = GetSubFolders(_basePath);
+            _newFileList = GetAllFiles(_basePath);
+
+            if (_oldFileList.SequenceEqual(_newFileList))
+            {
+                _workDown = true;
+
+                return;
+            }
+
+            /*-----「新增」差异文件夹-----*/
+            var addedFolderList = _newFolderList.Except(_oldFolderList).ToList();
+            if (addedFolderList.Count > 0)
+            {
+                foreach (var folder in addedFolderList)
+                {
+                    string remoteFolderPath = folder.Replace(_basePath, "");
+                    var result = FtpClient.CreateDir(remoteFolderPath);
+
+                    if (result.Contains("successfully"))
+                    {
+                        Console.WriteLine($"{DateTime.Now} - create folder {remoteFolderPath} OK.");
+                    }
+                }
+            }
+
+            /*-----「删除」差异文件夹-----*/
+            var deletedFolderList = _oldFolderList.Except(_newFolderList).ToList();
+            if (deletedFolderList.Count > 0)
+            {
+                foreach (var folder in deletedFolderList)
+                {
+                    string remoteFolderPath = folder.Replace(_basePath, "");
+                    var result = FtpClient.DeleteDir(remoteFolderPath);
+
+                    if (result.Contains("successfully"))
+                    {
+                        Console.WriteLine($"{DateTime.Now} - delete folder {remoteFolderPath} OK.");
+                    }
+                }
+            }
+
+            /*-----「新增」差异文件-----*/
+            var addedFileList = _newFileList.Except(_oldFileList).ToDictionary(x => x.Key, x => x.Value);
+            if (addedFileList.Count > 0)
+            {
+                foreach (var file in addedFileList)
+                {
+                    string localFilePath = file.Key;
+                    string remoteFilePath = localFilePath.Replace(_basePath, "");
+                    FtpClient.UploadFile(remoteFilePath, localFilePath);
+
+                    Console.WriteLine($"{DateTime.Now} - upload file {remoteFilePath} OK.");
+                }
+            }
+
+            /*-----「删除」差异文件-----*/
+            var deletedFileList = _oldFileList.Except(_newFileList).ToDictionary(x => x.Key, x => x.Value);
+            if (deletedFileList.Count > 0)
+            {
+                foreach (var file in deletedFileList)
+                {
+                    string filePath = file.Key;
+                    string remoteFilePath = filePath.Replace(_basePath, "");
+                    FtpClient.DeleteFile(remoteFilePath);
+
+                    Console.WriteLine($"{DateTime.Now} - delete file {remoteFilePath} OK.");
+                }
+            }
+            
+            _oldFolderList = new List<string>(_newFolderList);
+            _oldFileList = new Dictionary<string, string>(_newFileList);
+
+            _workDown = true;
+
+            Console.WriteLine("All Work down!!!");
+        }
+
+        /// <summary>
+        /// 获取子文件夹路径
+        /// </summary>
+        /// <param name="folderPath"></param>
+        static List<string> GetSubFolders(string folderPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            if (!dir.Exists)
+            {
+                return null;
+            }
+
+            var itemFiles = dir.GetDirectories().Select(x => x.FullName);
+            foreach (var item in itemFiles)
+            {
+                _newFolderList.Add(item);
+            }
+
+            //遍历子目录
+            var currentDirSubDirs = dir.GetDirectories().ToList();
+            currentDirSubDirs.ForEach(p => GetSubFolders(p.FullName));
+
+            return _newFolderList;
+        }
+
+        /// <summary>
+        /// 获取文件路径,md5
+        /// </summary>
+        /// <param name="folderPath"></param>
+        static Dictionary<string, string> GetAllFiles(string folderPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            if (!dir.Exists)
+            {
+                return null;
+            }
+
+            var itemFiles = dir.GetFiles().Where(x => x.Name != ".DS_Store").Select(x => x.FullName);
+            foreach (var item in itemFiles)
+            {
+                string fileMd5 = GetMD5Hash(item);
+
+                _newFileList.Add(item, fileMd5);
+            }
+
+            //遍历子目录
+            var currentDirSubDirs = dir.GetDirectories().ToList();
+            currentDirSubDirs.ForEach(p => GetAllFiles(p.FullName));
+
+            return _newFileList;
+        }
+
+        /// <summary>
+        /// 获取文件的MD5码
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        private static string GetMD5Hash(string filePath)
+        {
+            try
+            {
+                FileStream file = new FileStream(filePath, FileMode.Open);
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetMD5Hash() fail, error:" + ex.Message);
+            }
+        }
+    }
+}
